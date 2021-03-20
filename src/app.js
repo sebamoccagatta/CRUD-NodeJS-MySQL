cd17c0db2fae389f76f260cb8c9eb3376a1b1078
sebamoccagatta <sebamoccagatta1@gmail.com>
2021-03-19 23:46:55 -0300

    add env vars

--- src/app.js
+++ src/app.js
@@ -21,11 +21,11 @@ app.use(
   myConnection(
     mysql,
     {
-      host: "localhost",
-      user: "root",
-      password: "root",
-      port: 3306,
-      database: "crudnodejsmysql",
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      port: process.env.DB_PORT,
+      database: process.env.DB_DATABASE,
     },
     "single"
   )
